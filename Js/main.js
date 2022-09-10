06a917d116157748cff76c59339da39f3af09900
Shubham Tandon <iamshubham316@gmail.com>
2022-09-10 13:54:03 +0530

    toggle btn

--- Js/main.js
+++ Js/main.js
@@ -2,3 +2,14 @@ function hi(){
 	document.getElementById("hi").innerHTML = "Hello by Js";
 	document.getElementById("hi1").innerHTML = "Hello by Js v2";
 }
+
+
+function toggleHi(){
+	count+=1;
+	if(count%2!=0){
+		document.getElementById("toggle").innerHTML = "Show Hello";
+	}
+	else{
+		document.getElementById("toggle").style.display = "hidden";
+	}
+}
